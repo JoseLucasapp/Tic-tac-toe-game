f921d28452a8e082c50493f8094c6823039319b0
JoseLucasapp <josluc2016@gmail.com>
2020-09-24 21:26:14 -0300

    simple changes

--- script/startscript.js
+++ script/startscript.js
@@ -8,13 +8,3 @@ optionsPageOff = () => {
     optionspage.style.zIndex = -1;
     optionspage.style.opacity = 0;
 }
-changeBackgroundbefore = () =>{
-    document.body.style.backgroundImage = url('../img/space.gif');
-    document.body.style.backgroundPosition ='center';
-    document.body.style.backgroundSize = '100%';
-}
-changeBackgroundafter = () =>{
-    document.body.style.backgroundImage = url("../img/wather.gif");
-    document.body.style.backgroundPosition = 'center';
-    document.body.style.backgroundSize = '100%';
-}
