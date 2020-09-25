3fcd6cc4667ae24ba058bbaa8d0f50b6a17867aa
JoseLucasapp <josluc2016@gmail.com>
2020-09-24 21:16:06 -0300

    updates

--- script/startscript.js
+++ script/startscript.js
@@ -1,4 +1,3 @@
-click = 0;
 optionsPageOn = () => {
     let optionspage = document.getElementById('optionspage');
     optionspage.style.zIndex = 1;
@@ -10,22 +9,12 @@ optionsPageOff = () => {
     optionspage.style.opacity = 0;
 }
 changeBackgroundbefore = () =>{
-    backgrounds = ['../img/space.gif','../img/sky.gif','../img/earth.gif'];
-    click ++;
-    if(click >= 2){
-        click = 2;
-    }
     document.body.style.backgroundImage = url('../img/space.gif');
     document.body.style.backgroundPosition ='center';
     document.body.style.backgroundSize = '100%';
 }
 changeBackgroundafter = () =>{
-    backgrounds = ['../img/space.gif','../img/sky.gif','../img/earth.gif'];
-    click --;
-    if(click <= 0){
-        click = 0;
-    }
-    document.body.style.backgroundImage = url("../img/space.gif");
+    document.body.style.backgroundImage = url("../img/wather.gif");
     document.body.style.backgroundPosition = 'center';
-    document.body.style.backgroundSize = 100+'%';
+    document.body.style.backgroundSize = '100%';
 }
