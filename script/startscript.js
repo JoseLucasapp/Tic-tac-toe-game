570d22ebe7d1518a91aa816fb316161faa8b541f
JoseLucasapp <josluc2016@gmail.com>
2020-09-24 21:02:00 -0300

    add buttons

--- script/startscript.js
+++ script/startscript.js
@@ -1,3 +1,4 @@
+click = 0;
 optionsPageOn = () => {
     let optionspage = document.getElementById('optionspage');
     optionspage.style.zIndex = 1;
@@ -8,6 +9,23 @@ optionsPageOff = () => {
     optionspage.style.zIndex = -1;
     optionspage.style.opacity = 0;
 }
-changeBackground = () =>{
-    backgrounds = ['../img/space.gif','../img/volcano.gif','../img/wather.gif']
+changeBackgroundbefore = () =>{
+    backgrounds = ['../img/space.gif','../img/sky.gif','../img/earth.gif'];
+    click ++;
+    if(click >= 2){
+        click = 2;
+    }
+    document.body.style.backgroundImage = url('../img/space.gif');
+    document.body.style.backgroundPosition ='center';
+    document.body.style.backgroundSize = '100%';
+}
+changeBackgroundafter = () =>{
+    backgrounds = ['../img/space.gif','../img/sky.gif','../img/earth.gif'];
+    click --;
+    if(click <= 0){
+        click = 0;
+    }
+    document.body.style.backgroundImage = url("../img/space.gif");
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundSize = 100+'%';
 }
