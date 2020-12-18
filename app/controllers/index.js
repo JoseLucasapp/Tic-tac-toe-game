ecfa9fad42df33dd0f500a4079d92c77871d4aed
JoseLucasapp <josluc2016@gmail.com>
2020-12-18 15:22:22 -0300

    add controllers

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+module.exports.index_to_game = (application, req, res)=>{
+    res.render('index');
+}
+
+module.exports.game = (application, req, res)=>{
+    res.render('game');
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
+const app = require("../../config/server");
+
 module.exports = (application)=>{
     application.get('/',(req,res)=>{
-        res.render('index');
+        application.app.controllers.index.index_to_game(application, req, res);
+    });
+
+    application.get('/game', (req, res)=>{
+        application.app.controllers.index.game(application, req, res);
     });
 }
